d224130b697a646143e676558d86e389521344cc
Rohin Singh <rohinalexandersingh@gmail.com>
2023-02-01 21:23:58 -0800

    this is a code to determine which of two numbers is greater

---
+++ greater_smaller.py
@@ -0,0 +1,7 @@
+number1=int(input('enter 1 number'))
+number2=int(input('enter one more number'))
+if number1>number2:
+    print(number1,"is greater than",number2)
+
+else:
+    print(number1,"is smaller than",number2)
